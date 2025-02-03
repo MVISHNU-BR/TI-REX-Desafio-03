cee6e8df27e6e483323d3ae84cd5c2bb1c0e88c3
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-02-03 15:17:17 -0300

    Feat: inicio do projeto, configurações gerais.

---
+++ src/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-blue-200 text-3xl">teste de react</h1>
+
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
