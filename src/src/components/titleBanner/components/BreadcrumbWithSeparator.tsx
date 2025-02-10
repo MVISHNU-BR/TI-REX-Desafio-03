b242377a98b4d6f2ac9bd10d78b0fac68c0e6e14
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-02-10 15:24:02 -0300

    feat fix beadcrumb location page

--- src/src/components/titleBanner/components/BreadcrumbWithSeparator.tsx
+++ src/src/components/titleBanner/components/BreadcrumbWithSeparator.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 import {
   Breadcrumb,
@@ -10,6 +10,29 @@ import {
 } from "@/components/ui/breadcrumb";
 
 export function BreadcrumbWithSeparator() {
+  const location = useLocation();
+  const pathName = location.pathname;
+  const getPageName = (pathName: string) => {
+    switch (pathName) {
+      case "/components":
+        return "Components";
+      case "/shop":
+        return "Shop";
+      case "/aboult":
+        return "Aboult";
+      case "/contact":
+        return "contact";
+      case "/cart":
+        return "Cart";
+      case "/checkout":
+        return "Checkout";
+      default:
+        return "Unknown"; // Página desconhecida
+    }
+  };
+
+  const currentPageName = getPageName(pathName);
+
   return (
     <Breadcrumb>
       <BreadcrumbList className=" text-base font-medium text-black">
@@ -21,7 +44,7 @@ export function BreadcrumbWithSeparator() {
         <BreadcrumbSeparator />
         <BreadcrumbItem>
           <BreadcrumbLink>
-            <Link to="/components">Components</Link>
+            <Link to={pathName}>{currentPageName}</Link>
           </BreadcrumbLink>
         </BreadcrumbItem>
       </BreadcrumbList>
