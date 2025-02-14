bcb714c28541a8bd986a04784524935ca565a6b1
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-02-14 17:06:47 -0300

    fix .

--- src/src/pages/homePage/components/ImageSection.tsx
+++ src/src/pages/homePage/components/ImageSection.tsx
@@ -1,67 +1,17 @@
 const ImagesSection = () => {
   return (
-    <div className="flex mt-[140px] mx-auto mb-[50px] relative">
-      <div className="absolute left-1/2 transform -translate-x-1/2 top-[-40px]">
-        <p className="text-xl font-semibold text-[#616161] text-center">
+    <div className="flex flex-col items-center mt-[67px]mx-auto mb-[50px]">
+      <div className="flex flex-col items-center gap-2">
+        <p className="font-semibold text-fadeGray2 text-xl">
           Share your setup with
         </p>
         <h2 className="font-bold text-[40px]">#FuniroFurniture</h2>
       </div>
       <div className="">
-        <div className="flex gap-4 items-end">
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/prateleira.png"
-            alt=""
-            className="h-[382px]"
-          />
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/notebook.png"
-            alt=""
-            className=" h-[312px]"
-          />
-        </div>
-        <div className="flex gap-4 mt-4">
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/oldchair.png"
-            alt=""
-          />
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/vaso.png"
-            alt=""
-            className="h-[242px]"
-          />
-        </div>
-      </div>
-      <img
-        src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/tablenChairs2.png"
-        alt=""
-        className="h-[392px] mx-4 my-auto"
-      />
-      <div>
-        <div className="flex gap-4 items-end">
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/bed.png"
-            alt=""
-            className="h-[348px]"
-          />
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/TableAndChairs.png"
-            alt=""
-            className="h-[433px]"
-          />
-        </div>
-        <div className="flex gap-4 mt-4">
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/portrait.png"
-            alt=""
-            className="h-[242px]"
-          />
-          <img
-            src="https://imagesrojectsren999.s3.us-east-2.amazonaws.com/ItemImgs/shelf.png"
-            alt=""
-            className="h-[196px]"
-          />
-        </div>
+        <img
+          src="https://furniroimages.s3.us-east-2.amazonaws.com/sectionBanner.png"
+          alt="section banner"
+        />
       </div>
     </div>
   );
